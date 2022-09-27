20d90cf40481b6088c08260fe35d5b89715e12f7
Jin Andrei Parcia <77042439+pinktoothbrush123@users.noreply.github.com>
2022-09-27 22:48:37 +0800

    Add files via upload

---
+++ Graduate.java
@@ -0,0 +1,23 @@
+public class Graduate {
+
+        private String type;
+
+        Graduate(){
+        }
+
+        String getType() {
+            return this.type;
+        }
+
+        void setType() {
+            this.type = type;
+        }
+
+        void minYearsLeft(){
+            return;
+        }
+
+}
+
+
+
---
+++ Undergraduate.java
@@ -0,0 +1,20 @@
+public class Undergraduate {
+
+    private String status;
+
+    Undergraduate(){
+    }
+
+    String getStatus() {
+        return this.status;
+    }
+
+    void setStatus() {
+        this.status = status;
+    }
+
+    void minYearsLeft(){
+        return;
+    }
+
+}
